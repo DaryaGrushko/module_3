80b08d5fe34e2e09857781ce9d9a5229dbbbaa39
DaryaGrushko <daryagrushko@gmail.com>
2024-11-08 16:32:13 +0300

    Add files via upload

---
+++ module_3_5_2.py
@@ -0,0 +1,14 @@
+# Рекурсия
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return (first * get_multiplied_digits(int(str_number[1:])))
+    else:
+        return first
+
+
+print(get_multiplied_digits(40203))
+print(get_multiplied_digits(5754680674))
+print(get_multiplied_digits(1))
